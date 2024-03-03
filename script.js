69a02fcf67465d38ba9261a20896e5cb78fc02eb
zsoltcsengeri <zsoltcsengeri@yahoo.com>
2024-03-03 20:19:10 +0000

    escape quotes in csv

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ fetch("https://jsonplaceholder.typicode.com/posts")
 
 // save as a *.csv
 
-function exportCSV(arrayOfData) {
+/*function exportCSV(arrayOfData) {
   let CSV = "id;titleAndBody;" + "\n "; // data headers, the column headers in the csv file
   for (let i = 0; i < arrayOfData.length; i++ ){
     let row = `${arrayOfData[i][0]};${arrayOfData[i][1]};\n`
@@ -38,4 +38,25 @@ link.download= fileName + ".csv"
 link.textContent = "Click to downoad the csv"
 document.body.appendChild(link)
 
+}*/
+
+function exportCSV(arrayOfData) {
+  let CSV = '"id";"titleAndBody"\n'; // Column headers with quotes
+  for (let i = 0; i < arrayOfData.length; i++) {
+    // Enclose each field in quotes and replace internal quotes with two quotes
+    let row = `"${arrayOfData[i][0]}";"${arrayOfData[i][1].replace(/"/g, '""')}"\n`;
+    CSV += row;
+  }
+
+  // The rest of your original code to create the download link remains the same
+  const fileName = "my_data";
+  let csv = "\ufeff" + CSV;
+  let url = "data:text/csv;charset=utf-8,%EF%BB%BF" + encodeURI(csv);
+  let link = document.createElement("a");
+  link.href = url;
+  link.download = fileName + ".csv";
+  link.textContent = "Click to download the csv";
+  document.body.appendChild(link);
 }
+
+
